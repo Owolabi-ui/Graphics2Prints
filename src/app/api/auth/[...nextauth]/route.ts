9831ac0463245959c9e5d3327b093be8560837eb
Ezekiel Orogbesan <ezekielorogbesan@gmail.com>
2025-06-14 11:32:58 +0100

    fixed bugs

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -94,5 +94,5 @@ export const authOptions: NextAuthOptions = {
     }
   }
   
-  const handler = NextAuth(authOptions)
-  export { handler as GET, handler as POST }
+  const handler = NextAuth(authOptions);
+  export { handler as GET, handler as POST };
